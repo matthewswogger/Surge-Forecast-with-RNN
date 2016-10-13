3ef8dce7f610f38ae0cc4b6fd077d749acf8ddef
Matthew Swogger <matthewswogger@yahoo.com>
2016-10-12 23:36:36 -0700

    pretty graphs

---
+++ extra_trees/plotter.py
@@ -0,0 +1,60 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+
+class split_for_plot:
+    def __init__(self,predicted_values, true_values):
+        '''
+        Went ahead and used this to split the predicted_values into two
+        different dictionaries so that they can be graphed in sets of eight
+        instead of 16 within a jupyter notebook, this works best visually.
+        '''
+        self.predicted_values=predicted_values
+        self.true_values=true_values
+        self.first_8 = {}
+        self.second_8 = {}
+        count = 1
+        for point, value in self.predicted_values.iteritems():
+            if count <= 8:
+                self.first_8[point]=value
+            else:
+                self.second_8[point]=value
+            count+=1
+
+    def plot_1(self):
+        '''
+        This is for the first 8 points to be plotted, they are not in any
+        order due to using dictionaries.
+        '''
+        plt.xkcd() # makes it pretty
+        plt.figure(figsize=(20,25))
+        count=1
+        subs = {1:811, 2:812, 3:813, 4:814, 5:815, 6:816, 7:817, 8:818}
+        for point, values in self.first_8.iteritems():
+            if count is 9:
+                break
+            plt.subplot(subs[count])
+            plt.plot(values,label='3 min forecast',color='k')
+            plt.plot(self.true_values[point]['y_hold_out'],label='true {}'.format(point))
+            plt.legend()
+            count += 1
+        plt.show()
+
+    def plot_2(self):
+        '''
+        This is for the second 8 points to be plotted, they are not in any
+        order due to using dictionaries.
+        '''
+        plt.xkcd() # makes it pretty
+        plt.figure(figsize=(20,25))
+        count=1
+        subs = {1:811, 2:812, 3:813, 4:814, 5:815, 6:816, 7:817, 8:818}
+        for point, values in self.second_8.iteritems():
+            if count is 9:
+                break
+            plt.subplot(subs[count])
+            plt.plot(values,label='3 min forecast',color='k')
+            plt.plot(self.true_values[point]['y_hold_out'],label='true {}'.format(point))
+            plt.legend()
+            count += 1
+        plt.show()
---
+++ extra_trees/train_models.py
@@ -0,0 +1,69 @@
+import numpy as np
+import pandas as pd
+from sklearn.ensemble import ExtraTreesRegressor
+
+class train_models:
+    def __init__(self,data_dict):
+        self.data_dict=data_dict
+
+
+    def fit(self):
+        '''
+        Trains our entire model. Our model consits of 16 seperate
+        ExtraTreesRegressors, one for each point, with 128 trees in them.
+        Once a model is trained it is placed into a dictionary as the value
+        where the keys to access it is the point name.
+        '''
+        def to_train(X, y):
+            '''
+            Returns one ExtraTreesRegressor model.
+            '''
+            return ExtraTreesRegressor(n_estimators=128).fit(X, y)
+
+        self.model_dict = {}
+        for key, value in self.data_dict.iteritems():
+            self.model_dict[key] = to_train(value['X_train'], value['y_train'])
+
+
+    def predict_on_test_sets(self):
+        '''
+        Does a prediction with all 16 models on the test set and stores them
+        in a dictionary similar to the way the models were stored.
+        '''
+        self.predict_test_dict = {}
+        for point, model in self.model_dict.iteritems():
+            self.predict_test_dict[point] = model.predict(self.data_dict[point]['X_test'])
+        return self.predict_test_dict
+
+
+    def predict_on_hold_out_sets(self):
+        '''
+        Does a prediction with all 16 models on the hold out set and stores
+        them in a dictionary similar to the way the models were stored. This
+        is the more interesting prediction because it is still in timeseries
+        format.
+        '''
+        self.predict_hold_out_dict = {}
+        for point, model in self.model_dict.iteritems():
+            self.predict_hold_out_dict[point] = model.predict(self.data_dict[point]['X_hold_out'])
+        return self.predict_hold_out_dict
+
+
+    def mse_on_test_sets(self):
+        '''
+        Computes the mean squared error for all 16 test sets and then
+        averages them to give us the overall mean squared error of the
+        entire model.
+        '''
+        return np.mean([np.mean((prediction - self.data_dict[point]['y_test'])**2) \
+                                                        for point, prediction in self.predict_test_dict.iteritems()])
+
+
+    def mse_on_hold_out_sets(self):
+        '''
+        Computes the mean squared error for all 16 hold out sets and then
+        averages them to give us the overall mean squared error of the
+        entire model.
+        '''
+        return np.mean([np.mean((prediction - self.data_dict[point]['y_hold_out'])**2) \
+                                                    for point, prediction in self.predict_hold_out_dict.iteritems()])
