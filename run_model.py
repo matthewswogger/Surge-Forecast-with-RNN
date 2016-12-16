3e2989ec2575f95d58961ece206245d190d3fa6f
Matthew Swogger <matthewswogger@yahoo.com>
2016-12-15 17:43:03 -0800

    code cleanup

--- run_model.py
+++ run_model.py
@@ -12,7 +12,7 @@ def graph_em(test, forecast, graph_name):
     plt.figure(figsize=(16,8))
     for i, point in enumerate(test.T):
         plt.subplot(subs[i])
-        plt.title('History='+str(steps_of_history)+', Future='+str(steps_in_future))
+        plt.title('History='+str(STEPS_OF_HISTORY)+', Future='+str(STEPS_IN_FUTURE))
         plt.plot(point, 'k-', label='Actual')
         plt.plot(forecast[:,i], 'c-', label='Forecast')
         plt.legend()
@@ -38,28 +38,31 @@ sql_output = np.array([np.array(pd.read_sql('''WITH a AS (SELECT surge
 # this gets run when I'm done working for the session
 conn.close()
 
-steps_of_history = 30 # how far back in time I look
-steps_in_future = 5 # how far in future I forecast
+STEPS_OF_HISTORY = 30 # how far back in time I look
+STEPS_IN_FUTURE = 5 # how far in future I forecast
+FEATURES = 16 # number of features in model
+
+SPLIT = -1000 # where I make my train test split, this gives about 16.5 hours of test data
 
 # prepare data for model
-y = sql_output[steps_of_history+steps_in_future-1:,:]
+y = sql_output[STEPS_OF_HISTORY+STEPS_IN_FUTURE-1:,:]
 X = sql_output[:len(y),:]
-my_x = np.empty([len(y),steps_of_history,16])
+my_x = np.empty([len(y),STEPS_OF_HISTORY,FEATURES])
 
 for i, _ in enumerate(X):
-    time_chunk = X[i:i+steps_of_history,:]
-    if time_chunk.shape == (steps_of_history, 16):
+    time_chunk = X[i:i+STEPS_OF_HISTORY,:]
+    if time_chunk.shape == (STEPS_OF_HISTORY, FEATURES):
         my_x[i,:,:] = time_chunk
     else:
-        my_x[i,:,:] = np.ones((steps_of_history,16))
+        my_x[i,:,:] = np.ones((STEPS_OF_HISTORY,FEATURES))
 
-trainX, testX = my_x[:-1000,:], my_x[-1000:,:]
-trainY, testY = y[:-1000,:], y[-1000:,:]
+trainX, testX = my_x[:SPLIT,:], my_x[SPLIT:,:]
+trainY, testY = y[:SPLIT,:], y[SPLIT:,:]
 
 # Build my neural net
-net = tf.input_data(shape=[None, steps_of_history, 16])
+net = tf.input_data(shape=[None, STEPS_OF_HISTORY, FEATURES])
 net = tf.lstm(net, n_units=128, activation='softsign', return_seq=False)
-net = tf.fully_connected(net, 16, activation='linear')
+net = tf.fully_connected(net, FEATURES, activation='linear')
 net = tf.regression(net, optimizer='sgd', loss='mean_square', learning_rate=0.3)
 
 # Define model
@@ -74,22 +77,14 @@ model = tf.DNN(net, clip_gradients=0.0, tensorboard_verbose=0)
 # Load a model
 model.load("saved_model/150_epoch_act_softsign_nunits_128.tfl")
 
-
-# when the model forecasts that the surge will be LESS than 1 I correct it to say it is 1
+# predict
 predictY = np.array(model.predict(testX))
-corrected_predictions = np.zeros(predictY.shape)
 
-for i, time in enumerate(predictY):
-    for j, point in enumerate(time):
-        if point <=1.04:
-            corrected_predictions[i,j] = 1.0
-        else:
-            corrected_predictions[i,j] = point
+print ('')
+print ('Mean Squared Error: {}%'.format(round(np.mean((predictY - testY)**2)*100,2)))
+print ('')
 
-print (np.mean((predictY - testY)**2), np.mean((corrected_predictions - testY)**2))
+# print graphs of surge forecast
+graph_em(testY[:,:8], predictY[:,:8], 'forecast_1.png')
 
-
-
-graph_em(testY[:,:8], corrected_predictions[:,:8], 'forecast_1.png')
-
-graph_em(testY[:,8:], corrected_predictions[:,8:], 'forecast_2.png')
+graph_em(testY[:,8:], predictY[:,8:], 'forecast_2.png')
